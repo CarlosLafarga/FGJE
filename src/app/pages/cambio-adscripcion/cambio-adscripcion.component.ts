4564a2b46046f7eaf5fdbda00931e34fb6639a9c
mando2000 <mando_2000_02@hotmail.com>
2018-07-24 14:05:49 -0700

    Generacion de componente
    Se genero el componente de cambio de adscripcion.

---
+++ src/app/pages/cambio-adscripcion/cambio-adscripcion.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-cambio-adscripcion',
+  templateUrl: './cambio-adscripcion.component.html',
+  styleUrls: ['./cambio-adscripcion.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CambioAdscripcionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -31,6 +31,7 @@ import { FavoritesComponent } from '../theme/components/favorites/favorites.comp
 import { BlankComponent } from './blank/blank.component';
 import { SearchComponent } from './search/search.component';
 import { PruebaCarlosComponent } from './prueba/prueba-carlos.component';
+import { CambioAdscripcionComponent } from './cambio-adscripcion/cambio-adscripcion.component';
 
 
 @NgModule({
@@ -62,7 +63,8 @@ import { PruebaCarlosComponent } from './prueba/prueba-carlos.component';
     FavoritesComponent,
     BlankComponent,
     SearchComponent,
-    PruebaCarlosComponent
+    PruebaCarlosComponent,
+    CambioAdscripcionComponent
   ],
   providers:[
     {
