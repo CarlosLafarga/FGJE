6d519385d1c7fa6aeeab08c24fef90a5380f3ac0
Armando <Armando@DESKTOP-PCS6RE1>
2019-04-03 10:27:49 -0700

    mostrando mensaje de error de duplicados

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -30,14 +30,14 @@ export class UsuariosComponent implements OnInit {
       editButtonContent: '<i class="fa fa-pencil mr-3 text-primary"></i>',
       saveButtonContent: '<i class="fa fa-check mr-3 text-success"></i>',
       cancelButtonContent: '<i class="fa fa-times text-danger"></i>',
-      confirmSave:true
+      confirmSave: true
     },
     delete: {
       deleteButtonContent: '<i class="fa fa-trash-o text-danger"></i>',
       confirmDelete: true
     },
     noDataMessage: 'No hay registros que mostrar.',
-    
+
     columns: {
       usuario_id: {
         title: 'ID Usuario',
@@ -79,83 +79,83 @@ export class UsuariosComponent implements OnInit {
         editable: false,
         type: 'html',
         filter: false,
-        valuePrepareFunction: (value) => { 
-          if( value == 0 )
-            {return '<i class="fa fa-user mr-2 text-danger" [ngbTooltip]="Inactivo" placement="bottom" ></i>'}
-          else
-            { return '<i  class="fa fa-user mr-2 text-success" [ngbTooltip]="Inactivo" placement="bottom" ></i>'}
+        valuePrepareFunction: (value) => {
+          if (value == 0) { return '<i class="fa fa-user mr-2 text-danger" [ngbTooltip]="Inactivo" placement="bottom" ></i>' }
+          else { return '<i  class="fa fa-user mr-2 text-success" [ngbTooltip]="Inactivo" placement="bottom" ></i>' }
         }
       },
-      
+
     },
     pager: {
       display: true,
       perPage: 20
     }
   };
-  constructor( private usuarioService : UsuariosService) {
+  constructor(private usuarioService: UsuariosService) {
     this.getData((data) => {
       this.data = data;
     });
   }
+  
   public getData(data) {
     const req = new XMLHttpRequest();
     req.open('GET', this.usuarioService.API_URL + '/GetUsuario');
     req.onload = () => {
       data(JSON.parse(req.response));
-      
+
     };
     req.send();
   }
 
   public onDeleteConfirm(event): void {
-    
-    if(event.data.bEsActivo == 1){
-    swal({
-      title: 'Desactivar Usuario ',
-      text: '¿Esta seguro que desea desactivar al usuario?',
-      type: 'warning',
-      html: '<b>¿Esta seguro que desea desactivar al usuario?</b><br><label><b>Justificacion:</b></label>',
-      showCancelButton: true,
-      confirmButtonText: 'Desactivar',
-      cancelButtonText: 'Cancelar'
 
-     }).then((result) => {
+    if (event.data.bEsActivo == 1) {
+      swal({
+        title: 'Desactivar Usuario ',
+        text: '¿Esta seguro que desea desactivar al usuario?',
+        type: 'warning',
+        html: '<b>¿Esta seguro que desea desactivar al usuario?</b><br><label><b>Justificacion:</b></label>',
+        showCancelButton: true,
+        confirmButtonText: 'Desactivar',
+        cancelButtonText: 'Cancelar'
 
-      if (result.value) {
-        
-        console.log(event.data);
+      }).then((result) => {
 
-      } else if (result.dismiss === swal.DismissReason.cancel) {
-        
-      }
-    });
-  }else{
-    swal({
-      title:"Cuidado",
-      text: "Usuario ya se encentra desactivado.",
-      type: "warning"
-      }).then(() =>{
-      
-      // location.reload();
+        if (result.value) {
+
+          console.log(event.data);
+
+        } else if (result.dismiss === swal.DismissReason.cancel) {
+
+        }
       });
-  }
+    } else {
+      swal({
+        title: "Cuidado",
+        text: "Usuario ya se encentra desactivado.",
+        type: "warning"
+      }).then(() => {
+
+        // location.reload();
+      });
+    }
   }
 
   public onSaveConfirm(event) {
     if (window.confirm('Are you sure you want to save?')) {
       event.newData['name'] += ' + added in code';
       event.confirm.resolve(event.newData);
-      console.log("event newData",event.newData);
+      console.log("event newData", event.newData);
 
-      this.usuarioService.actualizarUsuario(event.newData.usuario_id,event.newData).subscribe(resp =>{
+      this.usuarioService.actualizarUsuario(event.newData.usuario_id, event.newData).subscribe(resp => {
         console.log(resp);
         alert("Usuario acualizado con exito!");
-      },err =>{
+      }, err => {
         console.log(err);
-        alert(err.message);
+        // alert(err.message);
+        alert(err.error.innerException.innerException.exceptionMessage);
       });
-      
+
     } else {
       event.confirm.reject();
     }
@@ -171,7 +171,7 @@ export class UsuariosComponent implements OnInit {
     //  }).then((result) => {
 
     //   if (result.value) {
-        
+
     //     console.log("aceptar",result.value);
     //     console.log("evento",event);
     //     event.confirm.resolve(event.data);
@@ -184,7 +184,7 @@ export class UsuariosComponent implements OnInit {
     //   }
     // });
 
-    
+
   }
 
   // public onRowSelect(event) {
@@ -199,10 +199,10 @@ export class UsuariosComponent implements OnInit {
   //   // console.log(event);
   // }
 
-  
+
 
   ngOnInit() {
-   
+
   }
 
 }
